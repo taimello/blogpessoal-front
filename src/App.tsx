805a36c7b3dc7de27b91b185d245f483ddfe1b6d
Taimello <tainarakarolina@gmail.com>
2023-07-26 15:13:12 -0300

    Introdução a Tailwind e Rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
-import Home from './paginas/home';
-import './App.css'
+import './App.css';
+import Home from './paginas/home/home';
+import Navbar from './components/navBar/NavBar';
+import Footer from './components/footer/Footer';
+import Login from './paginas/login/Login';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <Home />
-  );
+    <>
+    <BrowserRouter>
+        <Navbar />
+          <div className='min-h-[80vh]'>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/home" element={<Home />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+    </>
+);
 }
-
-export default App
+export default App;
