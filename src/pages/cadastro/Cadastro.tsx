56471fbf7e0247eb7ee525b47d3f3cce2a774c64
Taimello <tainarakarolina@gmail.com>
2023-08-02 10:40:08 -0300

    Resolvendo erros React5

--- src/pages/cadastro/Cadastro.tsx
+++ src/pages/cadastro/Cadastro.tsx
@@ -1,7 +1,7 @@
 import { ChangeEvent, useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import Usuario from '../../../models/Usuario'
-import { cadastrarUsuario} from 'src/services/Service.ts'
+import Usuario from '../../models/Usuario'
+import { cadastrarUsuario } from '../../services/Service'
 import './Cadastro.css'
 
 function Cadastro() {
