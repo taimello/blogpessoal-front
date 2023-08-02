cac07c631075006b5507e2a1d7e31a43d1f6f664
Taimello <tainarakarolina@gmail.com>
2023-08-02 14:56:57 -0300

    crud postagem

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,7 @@
 import homeLogo from '../../assets/home.png'
 import './Home.css';
+import ListaPostagens from '../../components/postagens/listaPostagens/ListaPostagens';
+import ModalPostagem from '../../components/postagens/modalPostagem/ModalPostagem';
 
 function Home() {
     return (
@@ -11,7 +13,7 @@ function Home() {
               <p className='text-xl'>Expresse aqui seus pensamentos e opniões</p>
   
               <div className="flex justify-around gap-4">
-              
+              <ModalPostagem />
                 <button className='rounded bg-white text-blue-800 py-2 px-4'>Ver postagens</button>
               </div>
             </div>
@@ -22,7 +24,7 @@ function Home() {
             </div>
           </div>
         </div>
-        
+        <ListaPostagens />
       </>
     );
 }
